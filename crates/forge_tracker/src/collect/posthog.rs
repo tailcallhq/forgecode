154247bcc6e1de6b5831732801f7a4ae8ba082be
laststylebender14 <ran.mahadik14@gmail.com>
2026-05-11 15:54:48 +0530

    chore(http): defer HTTP client initialization until first use

--- crates/forge_tracker/src/collect/posthog.rs
+++ crates/forge_tracker/src/collect/posthog.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::sync::OnceLock;
 use std::time::Duration;
 
 use chrono::NaiveDateTime;
@@ -11,23 +12,28 @@ use super::super::Result;
 use super::Collect;
 use crate::Event;
 
+fn build_tracker_client() -> Client {
+    Client::builder()
+        .connect_timeout(Duration::from_secs(10))
+        .read_timeout(Duration::from_secs(30))
+        .pool_idle_timeout(Duration::from_secs(90))
+        .pool_max_idle_per_host(5)
+        .build()
+        .expect("Failed to build HTTP client for PostHog tracker")
+}
+
 pub struct Tracker {
     api_secret: &'static str,
-    client: Client,
+    client: OnceLock<Client>,
 }
 
 impl Tracker {
     pub fn new(api_secret: &'static str) -> Self {
-        // Configure HTTP client with connection pooling similar to forge_provider
-        let client = Client::builder()
-            .connect_timeout(Duration::from_secs(10))
-            .read_timeout(Duration::from_secs(30))
-            .pool_idle_timeout(Duration::from_secs(90))
-            .pool_max_idle_per_host(5)
-            .build()
-            .expect("Failed to build HTTP client for PostHog tracker");
+        Self { api_secret, client: OnceLock::new() }
+    }
 
-        Self { api_secret, client }
+    fn client(&self) -> &Client {
+        self.client.get_or_init(build_tracker_client)
     }
 }
 
@@ -96,7 +102,7 @@ impl Collect for Tracker {
     // TODO: move http request to a dispatch
     async fn collect(&self, event: Event) -> Result<()> {
         let request = self.create_request(event)?;
-        self.client.execute(request).await?;
+        self.client().execute(request).await?;
 
         Ok(())
     }
